f883708739822e4b2e4ea4f62977030a5c603930
MohammedGN <mohamedguendouzi08@gmail.com>
2026-06-09 00:10:04 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CardWeather } from "./Card/Card";
-
+const x = null
 const theme = createTheme({
   typography: {
     fontFamily: ["arabic"],
